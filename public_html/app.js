115ef30666a05b5a1e164afe0beafe867a11cf6d
jose d lopez <jose@devlabs.vc>
2019-05-10 14:46:28 -0400

    update

--- public_html/app.js
+++ public_html/app.js
@@ -1,10 +1,14 @@
+//for debugging
+console.log('Im in your console. Inspect Element > Console.');
+
+
 // Grab the intro element
 // https://developer.mozilla.org/en-US/docs/Web/API/Document/getElementById
 var intro = document.getElementById("intro");
 
-// Put some text in there
-intro.textContent = "Hi. I'm in here.";
-intro.textContent += "This just got added";
+console.log(intro);
 
-//for debugging
-console.log('Im in your console. Inspect Element > Console.');
+// Put some text in there
+//intro.textContent = "Hi. I'm in here.";
+//intro.textContent += "This just got added";
+
