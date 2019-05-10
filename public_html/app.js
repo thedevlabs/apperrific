9a2a3e31acb3cc9202a693ccd0d590e6e454bd40
jose d lopez <jose@devlabs.vc>
2019-05-10 17:44:38 -0400

    sample functionality that can be done via JS to manipulate the DOM and CSS

--- public_html/app.js
+++ public_html/app.js
@@ -1,14 +1,38 @@
 //for debugging
 console.log('Im in your console. Inspect Element > Console.');
 
+//wait for HTML to finish downloading
+document.addEventListener("DOMContentLoaded", function(){
 
-// Grab the intro element
-// https://developer.mozilla.org/en-US/docs/Web/API/Document/getElementById
-var myIntro = document.getElementById('intro');
+  // Grab the intro element and add some simple text dynamically
+  // https://developer.mozilla.org/en-US/docs/Web/API/Document/getElementById
+  var introElem = document.getElementById('intro');
 
-console.log(myIntro);
+  // Put some text in there
+	introElem.textContent = "Hi. I'm in here. ";
+  introElem.textContent += "This just got added. ";
+  introElem.textContent += "Added something else.";
 
-// Put some text in there
-//intro.textContent = "Hi. I'm in here.";
-//intro.textContent += "This just got added";
+  //debug out to the console
+  console.log(introElem);
 
+  //
+  //  Run the function below that will detect mousemovements in the Navigation area
+  //
+  // https://developer.mozilla.org/en-US/docs/Web/API/GlobalEventHandlers/onmouseover
+
+  const n = document.querySelector('nav');
+  n.onmouseover = logMouseOver;
+  n.onmouseout = logMouseOut;
+
+});
+
+function logMouseOver() {
+  var m = document.getElementById('mousy');
+  m.innerHTML = 'MOUSE OVER detected';
+}
+
+function logMouseOut() {
+  var m = document.getElementById('mousy');
+  m.innerHTML = 'MOUSE OUT detected';
+}
